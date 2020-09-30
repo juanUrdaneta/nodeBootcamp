d24ccbfa08c4c9f796b95248732117619319c019
juanUrdaneta <juankorus@gmail.com>
2020-09-30 20:19:01 -0300

    nodemailer and recover email endpoints set up

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -49,6 +50,8 @@ const userSchema = new mongoose.Schema({
         },
     },
     lastPasswordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -80,6 +83,18 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
     }
 
     return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+    this.passwordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+    console.log({ resetToken }, this.passwordResetToken);
+    return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
