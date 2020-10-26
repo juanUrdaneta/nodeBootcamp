9935e06038352adc28167f3b8ccc860f3be06f7c
juanUrdaneta <juankorus@gmail.com>
2020-10-25 21:48:07 -0300

    importing data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -23,10 +25,16 @@ mongoose
     });
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+    fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 const importData = async () => {
     try {
         await Tour.create(tours);
+        await User.create(users, { validateBeforeSave: false });
+        await Review.create(reviews);
         console.log('data loaded');
         process.exit();
     } catch (error) {
@@ -37,6 +45,8 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Tour.deleteMany();
+        await User.deleteMany();
+        await Review.deleteMany();
         console.log('data deleted');
         process.exit();
     } catch (error) {
